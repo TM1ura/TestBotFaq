5f06bbb5dab7c8f2ec3e6b005214b9257b2887bb
M1ura <rasgfrdgh@gmail.com>
2023-03-13 06:49:45 +0300

    Add exam calculator(needs fixing)

--- index.js
+++ index.js
@@ -1,86 +1,60 @@
-const TelegramBot = require("node-telegram-bot-api");
+const{Telegraf, Scenes, session} = require('telegraf')
 const kb = require("./keyboard");
 const rate = require("./messageAnalys");
 
-const token = "5815919263:AAETy0dty_fBQOloBHKVSfs_BOuikFOkHGo";
-const bot = new TelegramBot(token, { polling: true });
+require('dotenv').config()
 
-// Обработка команды Старт
-bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
+const preExamScene = require('./scenes')
 
-  bot.sendMessage(
-    chatId,
-    `Привет! Я могу проконсультировать тебя по любому вопросу. Просто спроси!`,
-    {
-      reply_markup: JSON.stringify({
-        keyboard: kb.mainKb
-      }),
-    }
-  );
+
+
+const { BOT_TOKEN } = process.env;
+if (!BOT_TOKEN) throw new Error('"BOT_TOKEN" env var is required!');
+const bot = new Telegraf(BOT_TOKEN);
+
+const stage = new Scenes.Stage([
+  preExamScene
+])
+bot.use(session())
+bot.use(stage.middleware())
+
+bot.start(async ctx => {
+  await ctx.reply(`Hello ${ctx.update.message.from.first_name}!\nЯ могу проконсультировать тебя по любому вопросу. Просто спроси!`,   
+  kb.mainKb)
 });
 
-// Обработка кнопок
-bot.onText(/FAQ/, (msg) =>{
-  const chatId = msg.chat.id;
+bot.hears("FAQ", async ctx => {
+  await ctx.reply('Просто напишите мне ваш вопрос и я постараюсь найти на него ответ или задайте его напрямую приемной комиссии по форме',
+  kb.formKb)
+});
+bot.hears("Полезная информация", async ctx => {
+  await ctx.reply('Выберите интересующую вас тему',
+  kb.helpKb)
+});
+bot.hears("Тест на профориентацию", async ctx => {
+  await ctx.reply('Тест на профориентацию в разработке');
+});
+bot.hears("Калькулятор ЕГЭ", async ctx => {
+  //await ctx.reply('Укажите ваши баллы за экзамен по русскому языку (только число, например: 40)');
+  await ctx.scene.enter('preExamWizard');
+})
 
-  bot.sendMessage(chatId,'Просто напишите мне "Вопрос: ваш вопрос" и я постараюсь найти на него ответ или задайте его напрямую приемной комиссии по форме',     
-  {
-    reply_markup: JSON.stringify({
-      inline_keyboard: kb.formKb
-    })
-  });
+bot.action('Declaration', async ctx => {
+  await ctx.editMessageText('Выберите уровень образования', kb.declarationKb)
+});
+bot.action('Consert', async ctx => {
+  await ctx.editMessageText('Выберите уровень образования', kb.consertKb)
+});
+bot.action('Back to help', async ctx => {
+  await ctx.editMessageText('Выберите интересующую вас тему', kb.helpKb)
 });
 
-bot.onText(/Полезная информация/, (msg) =>{
-  const chatId = msg.chat.id;
-
-  bot.sendMessage(chatId, 'Выберете интересующую вас тему',     
-  {
-    reply_markup: JSON.stringify({
-      inline_keyboard: kb.helpKb
-    })
-  });
+bot.on("message", async ctx => {
+  result = rate.rateAnswer(ctx.message.text);
+  await ctx.reply(result, kb.formKb)
 });
 
-bot.onText(/Тест на профориентацию/, (msg) =>{
-  const chatId = msg.chat.id;
+bot.launch()
 
-  bot.sendMessage(chatId, 'Тест на профориентацию в разработке');
-});
-
-bot.onText(/Вопрос: (.+)/, (msg, match) =>{
-  const chatId = msg.chat.id;
-  const textMsg = match[1];
-
-  result = rate.rateAnswer(textMsg);
- 
-  bot.sendMessage(chatId, result,     
-    {
-    reply_markup: JSON.stringify({
-      inline_keyboard: kb.formKb
-    })
-  })
-});
-
-// Обработка Callback'ов от inline-кнопок
-bot.on("callback_query", msg => {
-  const chatId = msg.message.chat.id;
-  if (msg.data == 'Declaration'){
-    bot.sendMessage(chatId, 'Выберите уровень образования:',     
-    {
-      reply_markup: JSON.stringify({
-        inline_keyboard: kb.declarationKb
-      })
-    });
-  } else if (msg.data == 'Consert'){
-    bot.sendMessage(chatId, 'Выберите уровень образования:',     
-    {
-      reply_markup: JSON.stringify({
-        inline_keyboard: kb.consertKb
-      })
-    });
-  };
-});
-
-bot.on('polling_error', (err) => console.log(err));
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
